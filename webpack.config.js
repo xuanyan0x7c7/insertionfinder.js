82611b675a6f149c0864736cdc40abeeac42c4ec
Xuan Yan <xuanyan@xuanyan.ws>
2018-11-28 15:03:41 +0800

    project config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = [
+  {
+    output: {
+      filename: '[name].js',
+      library: 'insertionfinder',
+      libraryTarget: 'commonjs2'
+    },
+    entry: {insertionfinder: './lib/index.mjs'},
+    target: 'node',
+    mode: 'production',
+    optimization: {minimize: false}
+  },
+  {
+    output: {
+      filename: '[name].min.js',
+      library: 'insertionfinder',
+      libraryTarget: 'commonjs2'
+    },
+    entry: {insertionfinder: './lib/index.mjs'},
+    target: 'node',
+    mode: 'production'
+  }
+]
